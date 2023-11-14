cf3b2865399ac5b483939d35da738452636807d7
rezakariimii <rezakariimii1997@gmail.com>
2023-11-15 00:41:45 +0330

    Theme with Light & Dark Mode

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+function App() {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
